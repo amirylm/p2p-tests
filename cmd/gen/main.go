9cbd8fdc1649ca6fd817e0a979f2917a97bdf2f9
Amir Yahalom <amir@blox.io>
2022-09-11 10:11:32 +0300

    content generator

---
+++ cmd/gen/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/amir-blox/p2p-tests/utils"
+	"gopkg.in/yaml.v3"
+	"os"
+	"path"
+	"sync"
+)
+
+func main() {
+	var validators, operators, maxGroups int
+	var format, outDir string
+	flag.IntVar(&validators, "v", 250, "number of validators")
+	flag.IntVar(&operators, "n", 50, "number of operators")
+	flag.IntVar(&maxGroups, "g", 25, "maximum number of groups")
+	flag.StringVar(&outDir, "out", "./", "output directory")
+	flag.StringVar(&format, "format", "yaml", "output format")
+	flag.Parse()
+
+	groups := utils.Generate(operators, validators, maxGroups)
+
+	ngroups := len(groups)
+	nvalidators := 0
+	for _, gr := range groups {
+		nvalidators += len(gr)
+	}
+	validatorsAvg := float64(nvalidators) / float64(ngroups)
+	st := stats{
+		ValidatorsCount: nvalidators,
+		OperatorsCount:  operators,
+		GroupsCount:     ngroups,
+		AvgInGroup:      validatorsAvg,
+	}
+
+	fmt.Println("stats:", st)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		f, err := os.OpenFile(path.Join(outDir, ".clusters.yaml"), os.O_RDWR|os.O_CREATE, 0755)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+		enc := yaml.NewEncoder(f)
+		defer enc.Close()
+		enc.SetIndent(2)
+		if err := enc.Encode(groups); err != nil {
+			panic(err)
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		f, err := os.OpenFile(path.Join(outDir, ".stats.yaml"), os.O_RDWR|os.O_CREATE, 0755)
+		if err != nil {
+			panic(err)
+		}
+		defer f.Close()
+		enc := yaml.NewEncoder(f)
+		defer enc.Close()
+		enc.SetIndent(2)
+		if err := enc.Encode(&st); err != nil {
+			panic(err)
+		}
+	}()
+
+	wg.Wait()
+}
+
+type stats struct {
+	ValidatorsCount int
+	OperatorsCount int
+	GroupsCount int
+	AvgInGroup float64
+}
---
+++ utils/generate.go
@@ -0,0 +1,112 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/bloxapp/ssv-spec/types"
+	"github.com/herumi/bls-eth-go-binary/bls"
+	"math/rand"
+	"sort"
+	"time"
+)
+
+
+//var (
+//	curveOrder = new(big.Int)
+//)
+
+func init() {
+	_ = bls.Init(bls.BLS12_381)
+	_ = bls.SetETHmode(bls.EthModeDraft07)
+	//curveOrder, _ = curveOrder.SetString(bls.GetCurveOrder(), 10)
+	rand.Seed(time.Now().UnixNano())
+}
+
+// Generate generates groups of operators and validators
+func Generate(ops, vals, maxGroups int) Groups {
+	operators := GenerateOperatorIDs(ops)
+	g := GenerateGroups(vals, operators, maxGroups)
+	return g
+}
+
+func GenerateOperatorIDs(n int) []types.OperatorID {
+	oids := make([]types.OperatorID, n)
+	for i := 0; i < n; i++ {
+		oids[i] = types.OperatorID(i+1)
+	}
+	return oids
+}
+
+type Share struct {
+	PK string
+	Operators Group
+}
+
+type Group [4]types.OperatorID
+
+func (g Group) ID() string {
+	ints := make([]int, 4)
+	for i, oid := range g {
+		ints[i] = int(oid)
+	}
+	sort.Ints(ints)
+	return fmt.Sprintf("%d_%d_%d_%d", ints[0], ints[1], ints[2], ints[3])
+}
+
+func (g *Group) Append(oid types.OperatorID) bool {
+	if oid == types.OperatorID(0) {
+		 return false
+	}
+	for i, o := range g {
+		if o == oid {
+			return false
+		}
+		if o == types.OperatorID(0) {
+			g[i] = oid
+			return true
+		}
+	}
+	return false
+}
+
+func RandGroup(operators int) Group {
+	g := Group{}
+	n := 4
+	for i := 0; i < n; i++ {
+		o := types.OperatorID(rand.Intn(operators) + 1)
+		for !g.Append(o) {
+			o = types.OperatorID(rand.Intn(operators) + 1)
+		}
+	}
+	return g
+}
+
+type Groups map[string][]Share
+
+func GenerateGroups(n int, operators []types.OperatorID, maxGroups int) Groups {
+	groups := make(Groups)
+	nOperators := len(operators)
+	for i := 0; i < n; i++ {
+		g := RandGroup(nOperators)
+		gid := g.ID()
+		shares, ok := groups[gid]
+		if !ok {
+			if maxGroups > 0 && len(groups) >= maxGroups {
+				// avoid creating another group if we reached to max groups
+				i--
+				continue
+			}
+			shares = make([]Share, 0)
+		}
+		groups[gid] = append(shares, GenerateShare(g))
+	}
+	return groups
+}
+
+func GenerateShare(group Group) Share {
+	sk := &bls.SecretKey{}
+	sk.SetByCSPRNG()
+	return Share{
+		PK: sk.GetPublicKey().SerializeToHexStr(),
+		Operators: group,
+	}
+}
